e7a9573d62b850144ba4d400c100c30c44b00ac2
hldrgn <tiagyn163@gmail.com>
2024-11-15 08:14:33 +0300

    Update laba2.27.cpp
    Дополнения:
    1)Добавлен русский язык
    2)Добавлен цикл, который запрашивает у пользователя информацию о том, хочет ли он продолжить работу с программой

--- 1 semester/labarotory work 2/laba2.27.cpp
+++ 1 semester/labarotory work 2/laba2.27.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #define size 8
 
-// Функция, которая выводит шахматную доску
 void PB(char b[size][size])
 {
     for (int i = 0; i < size; i += 1)
@@ -16,11 +16,9 @@ void PB(char b[size][size])
     printf("\n");
 }
 
-// Проверка, можно ли разместить ферзя в позиции(g, v)
 int IS(char b[size][size], int g, int v)
 {
     int i, j;
-    // Проверяем текущий столбец
     for (i = 0; i < g; i += 1)
     {
         if (b[i][v] == 'Q')
@@ -29,7 +27,6 @@ int IS(char b[size][size], int g, int v)
         }
     }
 
-    // Проверяем левую диагональ
     for (i = g, j = v; i >= 0 && j >= 0; i -= 1, j -= 1)
     {
         if (b[i][j] == 'Q')
@@ -38,7 +35,6 @@ int IS(char b[size][size], int g, int v)
         }
     }
 
-    // Проверяем правую диагональ
     for (i = g, j = v; i >= 0 && j < size; i -= 1, j += 1)
     {
         if (b[i][j] == 'Q')
@@ -50,7 +46,6 @@ int IS(char b[size][size], int g, int v)
     return 1;
 }
 
-//Функция для правильного расположения 8 ферзей
 int SNQ(char b[size][size], int g, int sr, int sc)
 {
     if (g == size)
@@ -59,19 +54,18 @@ int SNQ(char b[size][size], int g, int sr, int sc)
     }
     if (g == sr)
     {
-        return SNQ(b, g+1, sr, sc);
+        return SNQ(b, g + 1, sr, sc);
     }
     for (int v = 0; v < size; v += 1)
     {
         if (IS(b, g, v))
         {
-            b[g][v] = 'Q'; // Ставим ферзя
-            // Переходим на другой ряд
+            b[g][v] = 'Q';
             if (SNQ(b, g + 1, sr, sc))
             {
                 return 1;
             }
-            b[g][v] = '.'; // Убираем ферзя
+            b[g][v] = '.';
         }
     }
     return 0;
@@ -79,35 +73,42 @@ int SNQ(char b[size][size], int g, int sr, int sc)
 
 int main()
 {
+    setlocale(LC_ALL, "RU");
     char b[size][size];
-    for (int i = 0; i < size; i += 1)
-    {
-        for (int j = 0 ; j < size; j += 1)
+    char cont;
+    while (1) {
+        for (int i = 0; i < size; i += 1)
         {
-            b[i][j] = '.';
+            for (int j = 0; j < size; j += 1)
+            {
+                b[i][j] = '.';
+            }
+        }
+        int sg, sv;
+        do
+        {
+            printf("Введите начальную позицию для первого ферзя (строка и столбец от 0 до 7):\n");
+            scanf("%d %d", &sg, &sv);
+        } while ((sg < 0 or sg > 7) or (sv < 0 or sv > 7));
+        b[sg][sv] = 'Q';
+        printf("Начальная доска:\n");
+        PB(b);
+        if (!SNQ(b, 0, sg, sv))
+        {
+            printf("Нет решения!\n");
+        }
+        else
+        {
+            printf("Решение: \n");
+            PB(b);
+        }
+
+        printf("Хотите продолжить (y/n)? ");
+        scanf(" %c", &cont);
+        if (cont != 'y') {
+            break; 
         }
     }
-    int sg, sv;
-    // Ввод начальной позиции ферзя
-    do
-    {
-        printf("Введите начальную позицию для первого ферзя (строка и столбец от 0 до 7):\n");
-        scanf("%d %d", &sg, &sv);
-    } while ((sg < 0 or sg > 7) or (sv < 0 or sv > 7));
-    // Ставим первого ферзя
-    b[sg][sv] = 'Q';
-    // Выводим начальную доску
-    printf("Начальная доска:\n");
-    PB(b);
-    // Запускаем решение с позиции следующего ряда
-    if (!SNQ(b, 0, sg, sv)) 
-    {
-        printf("Нет решения!\n");
-    } 
-    else 
-    {
-        printf("Решение: \n");
-        PB(b);
-    }
+
     return 0;
 }
